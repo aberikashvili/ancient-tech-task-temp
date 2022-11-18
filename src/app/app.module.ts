dc0fe6bbdcc675f1105ca73757dde191386d363a
Arkadi Berikashvili <arkadi.berikashvili@valor-software.com>
2022-11-19 00:37:10 +0400

    remove commented out code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { HttpLink } from 'apollo-angular/http';
-// import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoxesComponent } from './boxes/boxes.component';
 import { GraphQLModule } from './graphql.module';
-// import { InMemoryCache } from '@apollo/client/core';
 
 @NgModule({
   declarations: [
@@ -17,25 +14,11 @@ import { GraphQLModule } from './graphql.module';
   ],
   imports: [
     BrowserModule,
-    // ApolloModule,
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule,
   ],
-  providers: [
-    // {
-    //   provide: APOLLO_OPTIONS,
-    //   useFactory(httpLink: HttpLink) {
-    //     return {
-    //       cache: new InMemoryCache(),
-    //       link: httpLink.create({
-    //         uri: 'https://48p1r2roz4.sse.codesandbox.io',
-    //       }),
-    //     };
-    //   },
-    //   deps: [HttpLink],
-    // },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
